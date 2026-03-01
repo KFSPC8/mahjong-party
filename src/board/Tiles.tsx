3e3d5865f76d102310eb67dae58dcbd98072a16b
KFSPC8\Arne <arne.95@hotmail.com>
2026-03-01 13:17:22 +0800

    Bundle frontend, backend and DB with docker compose

--- src/board/Tiles.tsx
+++
@@ -1,9 +0,0 @@
-import Tile from "./Tile";
-import React from "react";
-
-export default function Tiles({ tiles }: { tiles: string[] }) {
-  const elements = tiles.map((tile, index) => (
-    <Tile tile={tile} key={tile + index} />
-  ));
-  return <div className="rack">{elements}</div>;
-}

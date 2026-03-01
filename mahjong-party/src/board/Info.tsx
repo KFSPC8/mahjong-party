7a286041c9b7f698402a8b5d70059dc445475064
KFSPC8\Arne <arne.95@hotmail.com>
2026-03-01 17:52:21 +0800

    Added inline tile to prevailing wind

--- mahjong-party/src/board/Info.tsx
+++ mahjong-party/src/board/Info.tsx
@@ -1,7 +1,9 @@
 import React, { FunctionComponent } from "react";
 import { Player, Round } from "../mahjong";
+import Tile from "./Tile";
 
 const WINDS = ["East", "South", "West", "North"];
+const WIND_TILES = ["40东风", "41南风", "42西风", "43北风"];
 
 const Info: FunctionComponent<{ players: Player[]; round: Round }> = ({
   players,
@@ -11,7 +13,19 @@ const Info: FunctionComponent<{ players: Player[]; round: Round }> = ({
   const { wind, dealer, draws_left } = round;
   return (
     <div className="info">
-      <span className="infobox">Prevailing wind: {WINDS[wind]}</span>
+      <span className="infobox">
+        Prevailing wind: {WINDS[wind]} 
+        <Tile
+          tile={WIND_TILES[wind]}
+          inline={true}
+          style={{
+            width: "20px",
+            height: "22px",
+            marginLeft: "6px",
+            verticalAlign: "-20%",
+          }}
+        />
+      </span>
       <span className="infobox">Dealer: {players[dealer].name}</span>
       <span className="infobox">Draws left: {draws_left}</span>
       <span style={{ marginRight: "auto" }} />
--- mahjong-party/src/board/Tile.tsx
+++ mahjong-party/src/board/Tile.tsx
@@ -1,20 +1,27 @@
-import React, { MouseEventHandler } from "react";
+import React, { MouseEventHandler, CSSProperties } from "react";
 
 export default function Tile({
   tile,
   onClick,
   selected,
   inline,
+  style,
 }: {
   tile: string;
   onClick?: MouseEventHandler;
   selected?: boolean;
   inline?: boolean;
+  style?: CSSProperties;
 }) {
   const classes = ["tile"];
   selected && classes.push("selected");
   inline && classes.push("inline");
   return (
-    <span className={classes.join(" ")} data-tile={tile} onClick={onClick} />
+    <span
+      className={classes.join(" ")}
+      data-tile={tile}
+      onClick={onClick}
+      style={style}
+    />
   );
 }
